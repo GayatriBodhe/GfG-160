9a316126c2176f807390b9307e9c544086e9a0f8
GayatriBodhe <gayatrimbodhe12@gmail.com>
2025-07-13 23:51:20 +0530

    Day1 commit

---
+++ Day1.js
@@ -0,0 +1,16 @@
+//Second largest no. in array
+class Solution {
+    getSecondLargest(arr) {
+        let n = arr.length;
+        
+        arr.sort((a,b) => a - b);
+        
+        for(let i=n-2;i>=0;i--){
+            if (arr[i]!== arr[n-1]) {
+                return arr[i];
+            }
+        }
+        
+        return -1;
+    }
+}
